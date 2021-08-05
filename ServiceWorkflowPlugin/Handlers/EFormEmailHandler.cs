21f8028f37c51284e6dd0b34c73d92e625b2a155
René Schultz Madsen <rm@microting.com>
2021-08-05 11:09:48 +0000

    Adding changes to insert images and replace texts.

--- ServiceWorkflowPlugin/Handlers/EFormEmailHandler.cs
+++ ServiceWorkflowPlugin/Handlers/EFormEmailHandler.cs
@@ -22,7 +22,11 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+using System.Collections.Generic;
+using Amazon.S3.Model;
+using ImageMagick;
 using Microsoft.EntityFrameworkCore;
+using Microting.eForm.Helpers;
 using Microting.eForm.Infrastructure;
 using Microting.EformAngularFrontendBase.Infrastructure.Data;
 using Microting.eFormWorkflowBase.Infrastructure.Data.Entities;
@@ -74,14 +78,14 @@ namespace ServiceWorkflowPlugin.Handlers
                 sdkDbContext.Cases.SingleOrDefaultAsync(x => x.MicrotingCheckUid == workflowCase.CheckMicrotingUid);
             Site createdBySite = await sdkDbContext.Sites.SingleOrDefaultAsync(x => x.Id == _case.SiteId);
 
-            await GenerateReportAndSendEmail(createdBySite.LanguageId, createdBySite.Name, message.CaseId);
+            await GenerateReportAndSendEmail(createdBySite.LanguageId, createdBySite.Name, message.CaseId, workflowCase, _case);
 
             if (!string.IsNullOrEmpty(workflowCase.SolvedBy))
             {
                 Site site = await sdkDbContext.Sites.SingleOrDefaultAsync(x =>
                     x.Name == workflowCase.SolvedBy);
 
-                await GenerateReportAndSendEmail(site.LanguageId, site.Name, message.CaseId);
+                await GenerateReportAndSendEmail(site.LanguageId, site.Name, message.CaseId, workflowCase, _case);
             }
         }
 
@@ -122,13 +126,13 @@ namespace ServiceWorkflowPlugin.Handlers
             }
         }
 
-        private async Task GenerateReportAndSendEmail(int languageId, string userName, int caseId)
+        private async Task GenerateReportAndSendEmail(int languageId, string userName, int caseId, WorkflowCase workflowCase, Microting.eForm.Infrastructure.Data.Entities.Case _case)
         {
             var emailRecipient = await _baseDbContext.EmailRecipients.SingleOrDefaultAsync(x => x.Name == userName);
-            var caseDto = await _sdkCore.CaseLookupCaseId(caseId);
+            //var caseDto = await _sdkCore.CaseLookupCaseId(caseId);
             await using MicrotingDbContext sdkDbConetxt = _sdkCore.DbContextHelper.GetDbContext();
-            Language language = await sdkDbConetxt.Languages.SingleOrDefaultAsync(x => x.Id == languageId);
-            var replyElement = await _sdkCore.CaseRead((int)caseDto.MicrotingUId, (int)caseDto.CheckUId, language);
+            //Language language = await sdkDbConetxt.Languages.SingleOrDefaultAsync(x => x.Id == languageId);
+            //var replyElement = await _sdkCore.CaseRead((int)_case.MicrotingUid, (int)_case.MicrotingCheckUid, language);
             var assembly = Assembly.GetExecutingAssembly();
             var assemblyName = assembly.GetName().Name;
             var stream = assembly.GetManifestResourceStream($"{assemblyName}.Resources.Email.html");
@@ -143,32 +147,145 @@ namespace ServiceWorkflowPlugin.Handlers
             }
             html = html
                 .Replace("{{link}}",
-                    $"{await _sdkCore.GetSdkSetting(Settings.httpServerAddress)}/cases/edit/{caseId}/{caseDto.CheckListId}")
+                    $"{await _sdkCore.GetSdkSetting(Settings.httpServerAddress)}/plugins/workflow-pn/edit-workflow-case/{caseId}")
                 .Replace("{{text}}", "");
 
-            // Fix for broken SDK not handling empty customXmlContent well
-            var customXmlContent = new XElement("FillerElement",
-                new XElement("InnerElement", "SomeValue")).ToString();
 
-            // get report file
-            var filePath = await _sdkCore.CaseToPdf(
-                caseId,
-                replyElement.Id.ToString(),
-                DateTime.Now.ToString("yyyyMMddHHmmssffff"),
-                $"{await _sdkCore.GetSdkSetting(Settings.httpServerAddress)}/" +
-                "api/template-files/get-image/",
-                "pdf",
-                customXmlContent, language);
+            List<KeyValuePair<string, List<string>>> pictures = new List<KeyValuePair<string, List<string>>>();
+            Site createdBySite = await sdkDbConetxt.Sites.SingleOrDefaultAsync(x => x.Id == _case.SiteId);
 
-            if (!File.Exists(filePath))
+            SortedDictionary<string, string> valuePairs = new SortedDictionary<string, string>
             {
-                throw new Exception("Error while creating report file");
+                {"{created_by}", createdBySite.Name},
+                {"{created_date}", workflowCase.CreatedAt.ToString("dd-MM-yyyy")},
+                {"{incident_type}", workflowCase.IncidentType},
+                {"{incident_location}", workflowCase.IncidentPlace},
+                {"{incident_description}", workflowCase.Description.Replace("&", "&amp;")},
+                {"{incident_deadline}", workflowCase.Deadline?.ToString("dd-MM-yyyy")},
+                {"{incident_action_plan}", workflowCase.ActionPlan.Replace("&", "&amp;")},
+                {"{incident_solved_by}", workflowCase.SolvedBy},
+                {"{incident_status}", workflowCase.Status}
+            };
+
+
+            foreach (PicturesOfTask picturesOfTask in await _dbContext.PicturesOfTasks.Where(x => x.WorkflowCaseId == caseId).ToListAsync())
+            {
+                UploadedData uploadedData =
+                    await sdkDbConetxt.UploadedDatas.SingleOrDefaultAsync(x => x.Id == picturesOfTask.UploadedDataId);
+
+                FieldValue fieldValue =
+                    await sdkDbConetxt.FieldValues.SingleOrDefaultAsync(x =>
+                        x.UploadedDataId == picturesOfTask.UploadedDataId);
+
+                var list = new List<string>();
+
+                string fileName =
+                    $"{uploadedData.Id}_700_{uploadedData.Checksum}{uploadedData.Extension}";
+
+
+                string fileContent = "";
+                using GetObjectResponse response =
+                    await _sdkCore.GetFileFromS3Storage(fileName);
+                using var image = new MagickImage(response.ResponseStream);
+                fileContent = image.ToBase64();
+
+                string geoTag = "";
+                if (fieldValue.Latitude != null)
+                {
+                    geoTag =
+                        $"https://www.google.com/maps/place/{fieldValue.Latitude},{fieldValue.Longitude}";
+                }
+
+                list.Add(fileContent);
+                list.Add(geoTag);
+
+                pictures.Add(new KeyValuePair<string, List<string>>("Billeder af hændelsen", list));
             }
+
+            foreach (PicturesOfTaskDone picturesOfTask in await _dbContext.PicturesOfTaskDone.Where(x => x.WorkflowCaseId == caseId).ToListAsync())
+            {
+                UploadedData uploadedData =
+                    await sdkDbConetxt.UploadedDatas.SingleOrDefaultAsync(x => x.Id == picturesOfTask.UploadedDataId);
+
+                FieldValue fieldValue =
+                    await sdkDbConetxt.FieldValues.SingleOrDefaultAsync(x =>
+                        x.UploadedDataId == picturesOfTask.UploadedDataId);
+
+                var list = new List<string>();
+
+                string fileName =
+                    $"{uploadedData.Id}_700_{uploadedData.Checksum}{uploadedData.Extension}";
+
+
+                string fileContent = "";
+                using GetObjectResponse response =
+                    await _sdkCore.GetFileFromS3Storage(fileName);
+                using var image = new MagickImage(response.ResponseStream);
+                fileContent = image.ToBase64();
+
+                string geoTag = "";
+                if (fieldValue.Latitude != null)
+                {
+                    geoTag =
+                        $"https://www.google.com/maps/place/{fieldValue.Latitude},{fieldValue.Longitude}";
+                }
+
+                list.Add(fileContent);
+                list.Add(geoTag);
+
+                pictures.Add(new KeyValuePair<string, List<string>>("Billeder behandlet hændelse", list));
+            }
+
+            stream = assembly.GetManifestResourceStream($"{assemblyName}.Resources.report.docx");
+
+            Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), "results"));
+
+            string timeStamp = DateTime.Now.ToString("yyyyMMddHHmmssffff");
+            string resultDocument = Path.Combine(Path.GetTempPath(), "results",
+                $"{timeStamp}_{caseId}.docx");
+
+
+            using (var fileStream = File.Create(resultDocument))
+            {
+                stream.CopyTo(fileStream);
+            }
+
+
+            ReportHelper.SearchAndReplace(valuePairs, resultDocument);
+
+
+            ReportHelper.InsertImages(resultDocument, pictures);
+            string outputFolder = Path.Combine(Path.GetTempPath(), "results");
+
+            ReportHelper.ConvertToPdf(resultDocument, outputFolder);
+
+            string filePath = Path.Combine(Path.GetTempPath(), "results",
+                $"{timeStamp}_{caseId}.pdf");
+
+
+            // // Fix for broken SDK not handling empty customXmlContent well
+            // var customXmlContent = new XElement("FillerElement",
+            //     new XElement("InnerElement", "SomeValue")).ToString();
+            //
+            // // get report file
+            // var filePath = await _sdkCore.CaseToPdf(
+            //     caseId,
+            //     replyElement.Id.ToString(),
+            //     DateTime.Now.ToString("yyyyMMddHHmmssffff"),
+            //     $"{await _sdkCore.GetSdkSetting(Settings.httpServerAddress)}/" +
+            //     "api/template-files/get-image/",
+            //     "pdf",
+            //     customXmlContent, language);
+            //
+            // if (!File.Exists(filePath))
+            // {
+            //     throw new Exception("Error while creating report file");
+            // }
 
             await SendFileAsync(
                 "no-reply@microting.com",
                 userName,
-                "-",
+                $"{workflowCase.CreatedAt:dd-MM-yyyy}; {workflowCase.IncidentType}; {workflowCase.IncidentPlace}",
                 emailRecipient?.Email,
                 filePath,
                 html);
